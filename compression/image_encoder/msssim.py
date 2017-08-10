7ac4506e3f4de61d6827cfb7f2b5366c2c98b71c
Yetish Joshi <yetish.joshi@gmail.com>
2017-08-10 09:52:08 +0100

    Updated to read in as binary and not as UTF8
    Minor issue which was causing the image files to be treated as Unicode than as binary

--- compression/image_encoder/msssim.py
+++ compression/image_encoder/msssim.py
@@ -198,9 +198,9 @@ def main(_):
     print('\nCannot find --compared_image.\n')
     return
 
-  with tf.gfile.FastGFile(FLAGS.original_image) as image_file:
+  with tf.gfile.FastGFile(FLAGS.original_image, mode='rb') as image_file:
     img1_str = image_file.read()
-  with tf.gfile.FastGFile(FLAGS.compared_image) as image_file:
+  with tf.gfile.FastGFile(FLAGS.compared_image mode='rb') as image_file:
     img2_str = image_file.read()
 
   input_img = tf.placeholder(tf.string)
